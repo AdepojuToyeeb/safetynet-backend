629681a851c43abf6a5653182351a9eaf396f2ef
Amma <71434200+amarachi4423@users.noreply.github.com>
2024-10-08 13:30:02 +0100

    initial commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository} from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Users } from './users.entity';
+import { CreateUserDTO } from './create-user.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+constructor(
+    @InjectRepository(Users)
+     private usersRepository: Repository<Users>,
+){}
+ async createUser(CreateUserDTO: CreateUserDTO): Promise<Users>{
+    const {email,password} = CreateUserDTO;
+
+    const hashedPassword = await bcrypt.hash(password);
+    const newUser = this.usersRepository.create({email, password: hashedPassword});
+
+    return this.usersRepository.save(newUser);
+
+ }
+ async findbyEmail(email: string): Promise<Users | undefined> {
+    return this.usersRepository.findOneBy({email});
+ }
+
+}
